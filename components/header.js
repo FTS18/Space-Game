0b664b840580d695d00deae50d027c77f21175a9
FTS18 <anandsagardubey71568@gmail.com>
2023-04-26 12:11:24 +0530

    Updated Android Space Game

--- components/header.js
+++ components/header.js
@@ -332,7 +332,7 @@ nav .content .links{
   <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
   <div class="content">
   <div class="logo">
-  <a class="logo " href="index.html">
+  <a class="logo " href="index_real.html">
     <img id="svg" alt="" src="assets/images/logo.png" />
   </a>
   </div>
@@ -766,3 +766,369 @@ class Header1 extends HTMLElement {
     }
 }
 customElements.define('c-nav1', Header1);
+
+const headerTemplate2 = document.createElement('template');
+
+headerTemplate2.innerHTML = `
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Chivo+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  font-family: 'Chivo Mono', monospace;
+}
+body::-webkit-scrollbar {
+    width: 14px;
+    background: #111111;
+}
+
+body::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 0px rgba(0, 0, 0, 0.7);
+}
+
+body::-webkit-scrollbar-thumb {
+    background-color: #383838;
+    border: 3.8px solid #111111;
+    border-radius: 6px;
+    -webkit-border-radius: 6px;
+    -moz-border-radius: 6px;
+    -ms-border-radius: 6px;
+    -o-border-radius: 6px;
+}
+.wrapper{
+  background: #000;
+  position: fixed;
+  width: 100%;
+}
+.wrapper nav{
+  position: relative;
+  display: flex;
+  max-width: calc(100% - 200px);
+  margin: 0 auto;
+  height: 75px;
+  align-items: center;
+  justify-content: space-between;
+  width:100%;
+}
+nav .content{
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 1220px;
+  margin:0px auto;
+}
+nav .content .links{
+  margin-left: 530px;
+  display: flex;
+}
+.content .logo a{
+  color: #e7e7e7;
+  font-size: 30px;
+  font-weight: 600;
+}
+.content .links li{
+  list-style: none;
+  line-height: 70px;
+}
+.content .links li a,
+.content .links li label{
+  color: #e7e7e7;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 9px 17px;
+  border-radius: 5px;
+  transition: all 0.3s ease;
+}
+.content .links li label{
+  display: none;
+}
+.content .links li a:hover,
+.content .links li label:hover{
+  background: #111;
+}
+.content .links li .active{
+  color: #ff0040;
+}
+.wrapper .menu-icon{
+  color: #ff0040;
+  font-size: 18px;
+  cursor: pointer;
+  line-height: 70px;
+  width: 70px;
+  text-align: center;
+}
+.wrapper .menu-icon{
+  display: none;
+}
+.wrapper input[type="checkbox"]{
+  display: none;
+}
+.logo {
+    top: 3px;
+    left: 0px;
+    position: relative;
+    width: 120px;
+    height: auto;
+    -webkit-tap-highlight-color: transparent;
+    z-index: 10;
+    float: left;
+    transition: .35s ease-in-out;
+}
+
+#svg {
+  position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+}
+
+/* Dropdown Menu code start */
+.content .links ul{
+  position: absolute;
+  background: #000;
+  top: 80px;
+  z-index: -1;
+  opacity: 0;
+  visibility: hidden;
+}
+.content .links li:hover > ul{
+  top: 70px;
+  opacity: 1;
+  visibility: visible;
+  transition: all 0.3s ease;
+}
+.content .links ul li a{
+  display: block;
+  width: 100%;
+  line-height: 30px;
+  border-radius: 0px!important;
+}
+.content .links .active{
+  color: #ff0040;
+  background: #222;
+  margin-left: 15px;
+  margin-right: 20px;
+}
+.content .links ul ul{
+  position: absolute;
+  top: 0;
+  right: calc(-100% + 8px);
+}
+.content .links ul li{
+  position: relative;
+}
+.content .links ul li:hover ul{
+  top: 0;
+}
+
+.back img {
+  position: relative;
+  top: 0px;
+  left: 100%;
+  width: 25px;
+  filter: invert(100%);
+}
+.back {
+  color: #ff0040;
+  font-size: 18px;
+  cursor: pointer;
+  line-height: 70px;
+  width: 70px;
+  text-align: center;
+  position: relative;
+  top: 8px;
+  right: 50px;
+  margin-bottom: 0;
+  display: none;
+}
+/* Responsive code start */
+@media screen and (max-width: 1250px){
+  .wrapper nav{
+    max-width: 100%;
+    padding: 0 20px;
+  }
+
+  nav .content{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    max-width: 850px;
+    margin:0px auto;
+  }
+
+  nav .content .links{
+    margin-left: 250px;
+  }
+  .content .links li a{
+    padding: 8px 13px;
+  }
+}
+
+@media screen and (max-width: 900px){
+  .wrapper .menu-icon{
+    display: block;
+  }
+  .back{
+    display: block
+  }
+  .wrapper #show-menu:checked ~ .menu-icon i::before{
+    content: "\f00d";
+  }
+  nav .content .links{
+    display: block;
+    position: fixed;
+    background: #000;
+    height: 100%;
+    width: 100%;
+    top: 70px;
+    left: -100%;
+    margin-left: 0;
+    max-width: 300px;
+    overflow-y: auto;
+    padding-bottom: 100px;
+    transition: all 0.3s ease;
+  }
+  nav #show-menu:checked ~ .content .links{
+    left: 0%;
+  }
+  .content .links li{
+    margin: 15px 20px;
+  }
+  .content .links li a,
+  .content .links li label{
+    line-height: 40px;
+    font-size: 16px;
+    display: block;
+    padding: 8px 18px;
+    cursor: pointer;
+  }
+  .content .links .active{
+    color: #ff0040;
+    background: #111;
+    margin:0;
+  }
+  .content .links li a.desktop-link{
+    display: none;
+  }
+
+  /* dropdown responsive code start */
+  .content .links ul,
+  .content .links ul ul{
+    position: static;
+    opacity: 1;
+    visibility: visible;
+    background: none;
+    max-height: 0px;
+    overflow: hidden;
+  }
+  .content .links #show-features:checked ~ ul,
+  .content .links #show-services:checked ~ ul,
+  .content .links #show-items:checked ~ ul{
+    max-height: 100vh;
+  }
+  .content .links ul li{
+    margin: 7px 20px;
+  }
+  .content .links ul li a{
+    font-size: 18px;
+    line-height: 30px;
+    border-radius: 5px!important;
+  }
+  .logo{
+    top:2px;
+    width: 110px;
+    left: 50%;
+    transform: translate(-50%);
+  }
+}
+
+@media screen and (max-width: 400px){
+
+  .content .logo a{
+    font-size: 27px;
+  }
+  .wrapper .search-box{
+    max-width: calc(100% - 70px);
+  }
+  .wrapper .search-box input{
+    padding-right: 30px;
+  }
+}
+
+.dummy-text{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  z-index: -1;
+  padding: 0 20px;
+  text-align: center;
+  transform: translate(-50%, -50%);
+}
+.dummy-text h2{
+  font-size: 45px;
+  margin: 5px 0;
+}
+
+</style>
+<div class="wrapper">
+<nav>
+  <input type="checkbox" id="show-search">
+  <input type="checkbox" id="show-menu">
+  <label for="show-menu" class="menu-icon"><i class="fas fa-bars"></i></label>
+  <div class="content">
+  <div class="logo">
+  <a class="logo " href="index_real.html">
+    <img id="svg" alt="" src="assets/images/logo.png" />
+  </a>
+  </div>
+  <ul class="links">
+    <li><a class="active" href="index.html">Home</a></li>
+    <li>
+      <a href="#" class="desktop-link">Connect</a>
+      <input type="checkbox" id="show-services">
+      <label for="show-services">Connect</label>
+      <ul>
+        <li><a href="mailto:teamfinixx@gmail.com">Mail Us</a></li>
+        <li><a href="https://chat.whatsapp.com/ECHJEEU1w901D89UMd0c3m">Discuss</a></li>
+        <!---->
+      </ul>
+    </li>
+    <!-- class desktop-link to show only in desktop-->
+    <li><a href="coming-soon.html">Explore</a></li>
+    <li><a href="#" class="desktop-link">Developers</a>
+    <input type="checkbox" id="show-items">
+    <label for="show-items">Developers</label>
+    <ul>
+    <li><a href="https://github.com/fts18">Ananay</a></li>
+    <li><a href="https://github.com/Arunjay126">Arunjay</a></li>
+    </ul>
+  </li>
+    <li><a href="#feedback">Feedback</a></li>
+  </ul>
+  </div>
+  <div class="back">
+      <a href="./index_real">
+          <img alt="" class="go-icon" src="https://img.icons8.com/external-simple-solid-edt.graphics/100/000000/external-Back-arrows-simple-solid-edt.graphics.png" />
+      </a>
+  </div>
+</nav>
+</div>
+`;
+
+class Header2 extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+        shadowRoot.appendChild(headerTemplate2.content);
+    }
+}
+customElements.define('c-nav2', Header2);
