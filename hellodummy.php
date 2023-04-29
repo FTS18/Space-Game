8d38e8278a3386ddb2cb656ce11aa18d6694b316
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-30 03:57:21 +0530

    Delete hellodummy.php

--- hellodummy.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(isset($_POST['submit'])) {
-    $path = $_POST['path'];
-    $command = escapeshellcmd("finaldummy.py");
-    $output = shell_exec($command);
-    echo "<pre>$output</pre>";
-}
-?>
