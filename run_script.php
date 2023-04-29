216c58db94d1889aee2cdefbc0a02b3ec0e94a66
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-29 06:38:37 +0530

    Update run_script.php

--- run_script.php
+++ run_script.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST['submit'])) {
     $path = $_POST['path'];
-    $command = escapeshellcmd("pythonfiledummy.py");
+    $command = escapeshellcmd("finaldummy.py");
     $output = shell_exec($command);
     echo "<pre>$output</pre>";
 }
