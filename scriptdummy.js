62d81adb6ffd296c49a6f47ed7889e0d11dcb679
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-30 04:29:12 +0530

    Update scriptdummy.js

--- scriptdummy.js
+++ scriptdummy.js
@@ -1,11 +1,11 @@
-const { spawn } = require('child_process');
-
-const pyProg = spawn('python', ['finaldummy.py']);
-
-pyProg.stdout.on('data', function(data) {
-	console.log(data.toString());
-});
-
-pyProg.stderr.on('data', (data) => {
-	console.error(data.toString());
+const runBtn = document.getElementById('run-btn');
+runBtn.addEventListener('click', function() {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', 'script.php', true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			console.log(xhr.responseText);
+		}
+	};
+	xhr.send();
 });
