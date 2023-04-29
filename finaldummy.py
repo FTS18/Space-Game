ed8ddd7a9d04594d7f051c47101f5c81ce2bf36c
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-30 04:49:28 +0530

    Update finaldummy.py

--- finaldummy.py
+++ finaldummy.py
@@ -1,31 +1,4 @@
-import js2py
-a = 'arunjay'
-b = 1234
-# Load the JavaScript code from a file
-with open('datadummy.js', 'r') as f:
-    js_code = f.read()
+import sys
 
-# Create a JavaScript context using js2py
-context = js2py.EvalJs()
-
-# Execute the JavaScript code in the context
-context.execute(js_code)
-
-# Get the global object from the context
-global_object = context.this
-
-# Get the values of the variables
-foo_value = global_object.names
-bar_value = global_object.passwords
-
-# Print the values of the variables
-newna = list(foo_value) + [a]
-newpa= list(bar_value) + [b]
-new_js_code = f"""
-    const names = {newna}
-    const passwords = {newpa}
-"""
-      
-with open('datadummy.js', 'w') as f:
-    f.write(new_js_code)
-print('done')
+input_data = sys.argv[1] # get the input data from the command line argument
+print("Input data received: ", input_data)
