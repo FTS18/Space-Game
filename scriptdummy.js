3baf288e63f82d3bb12914894ccd54bdb1f91e61
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-30 04:54:12 +0530

    Update scriptdummy.js

--- scriptdummy.js
+++ scriptdummy.js
@@ -2,7 +2,7 @@ function runPythonScript() {
   const { spawn } = require('child_process');
 
   // Run a Python script called script.py with arguments
-  const pythonProcess = spawn('python', ['script.py', 'arg1', 'arg2']);
+  const pythonProcess = spawn('python', ['finaldummy.py', 'arg1', 'arg2']);
 
   // Log the output of the Python script to the console
   pythonProcess.stdout.on('data', (data) => {
