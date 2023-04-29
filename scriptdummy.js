75528af9d29185a01e2210067c539acfa3d0550e
Arunjay126 <72449359+Arunjay126@users.noreply.github.com>
2023-04-30 04:31:07 +0530

    Update scriptdummy.js

--- scriptdummy.js
+++ scriptdummy.js
@@ -1,7 +1,7 @@
 const runBtn = document.getElementById('run-btn');
 runBtn.addEventListener('click', function() {
 	const xhr = new XMLHttpRequest();
-	xhr.open('GET', 'script.php', true);
+	xhr.open('GET', 'scriptdummy.php', true);
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			console.log(xhr.responseText);
